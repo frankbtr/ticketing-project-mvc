97e9beeabfad1797c8a3751c04837b398d67b92e
Frank Batur <frank_batur@outlook.com>
2023-03-29 23:10:53 -0400

    delete project implementation is done.

--- src/main/java/com/frank/controller/ProjectController.java
+++ src/main/java/com/frank/controller/ProjectController.java
@@ -7,6 +7,7 @@ import com.frank.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -37,5 +38,11 @@ public class ProjectController {
       //  project.setProjectStatus(Status.OPEN);
         projectService.save(project);
         return "redirect:/project/create";
+    }
+
+    @GetMapping("/delete/{projectcode}")
+    public String deleteProject(@PathVariable("projectcode") String projectcode){
+        projectService.deleteById(projectcode);
+         return "redirect:/project/create";
     }
 }
